ffa335282bf13cf62fb3bae08699a8b6757b9c54
Daniel Wolf <daniel-wolf@outlook.com>
2023-10-29 20:59:41 +0100

    update

--- Controllers/JobPostingController.cs
+++ Controllers/JobPostingController.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
+using worktastic.Data;
 using worktastic.Models;
 
 namespace worktastic.Controllers;
 
 public class JobPostingController : Controller
 {
+
+    private readonly ApplicationDbContext _context;
+
+    public JobPostingController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
 
     public IActionResult Index()
     {
@@ -17,10 +25,43 @@ public class JobPostingController : Controller
         return View();
     }
 
-    public IActionResult CreateEditJob(JobPosting jobPosting)
+    public IActionResult CreateEditJob(JobPosting jobPosting, IFormFile file)
     {
-        Console.WriteLine("");
-        // TODO: write posting to db
+        if (file != null)
+        {
+            using var ms = new MemoryStream();
+            file.CopyTo(ms);
+            var bytes = ms.ToArray();
+            jobPosting.CompanyImage = bytes;
+        }
+
+        // default int is 0
+        if (jobPosting.Id == 0)
+        {
+            _context.JobPostings.Add(jobPosting);
+        }
+        else
+        {
+            var jobFromDb = _context.JobPostings.SingleOrDefault(x => x.Id == jobPosting.Id);
+            
+            if(jobFromDb == null){
+                return NotFound();
+            }
+
+            jobFromDb.CompanyImage = jobPosting.CompanyImage;
+            jobFromDb.CompanyName = jobPosting.CompanyName;
+            jobFromDb.Salary = jobPosting.Salary;
+            jobFromDb.ContactMail = jobPosting.ContactMail;
+            jobFromDb.ContactPhone = jobPosting.ContactPhone;
+            jobFromDb.ContactWebsite = jobPosting.ContactWebsite;
+            jobFromDb.JobTitle = jobPosting.JobTitle;
+            jobFromDb.JobDescription = jobPosting.JobDescription;
+            jobFromDb.JobLocation = jobPosting.JobLocation;
+            jobFromDb.StartDate = jobPosting.StartDate;
+        }
+
+    
+        _context.SaveChanges();
         return RedirectToAction("Index");
     }
 }
