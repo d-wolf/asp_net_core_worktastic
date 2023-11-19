140ad594527be162235356cfbcb1eb858fddef0b
Daniel Wolf <daniel-wolf@outlook.com>
2023-11-19 16:51:20 +0100

    Create ApiJobPostingController.cs

---
+++ Controllers/ApiJobPostingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using worktastic.Data;
+using worktastic.Models;
+
+namespace worktastic.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApiJobPostingController : ControllerBase {
+ 
+    [HttpGet("")] //Matches GET api/admin <-- Would also work with [HttpGet]
+    public IActionResult Get() {
+        return Ok();
+    }
+
+    [HttpGet("{id}")] //Matches GET api/admin/5
+    public IActionResult Get(int id) {
+        return Ok("value");
+    }    
+
+    //...other code removed for brevity
+}
